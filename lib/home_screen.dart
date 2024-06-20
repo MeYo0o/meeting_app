bdcd242e158b6adfa07ffd32961b59a281d05dd9
Moaz Ahmed <justmeyo2025@gmail.com>
2024-06-20 19:24:04 +0300

    update: refactor HomeScreen.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+import 'package:meeting_app/helper_methods/helper_methods.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class HomeScreen extends StatefulWidget {
@@ -16,10 +17,11 @@ class _HomeScreenState extends State<HomeScreen> {
   stt.SpeechToText speech = stt.SpeechToText();
   bool isTakingAudioInput = false;
   bool servicesAreEnabled = false;
+  bool forceStopped = false;
 
-  bool audioMuted = true;
-  bool videoMuted = true;
-  bool screenShareOn = false;
+  // bool audioMuted = true;
+  // bool videoMuted = true;
+  // bool screenShareOn = false;
   List<String> participants = [];
   final _jitsiMeetPlugin = JitsiMeet();
 
@@ -30,200 +32,18 @@ class _HomeScreenState extends State<HomeScreen> {
     startSTTServices();
   }
 
-  @override
-  void dispose() {
-    messageController.dispose();
-    super.dispose();
-  }
-
   void startSTTServices() async {
     servicesAreEnabled = await speech.initialize(
       onStatus: (status) {
         log('status: $status');
+        if (status == 'done' && !forceStopped) {
+          startCaptureAudio();
+        }
       },
       onError: (errorNotification) {
         log('onError: $errorNotification');
       },
     );
-  }
-
-  Future<void> join() async {
-    var options = JitsiMeetConferenceOptions(
-      room: 'ClinicalHallsPassAround',
-      configOverrides: {
-        'startWithAudioMuted': true,
-        'startWithVideoMuted': true,
-      },
-      featureFlags: {
-        FeatureFlags.addPeopleEnabled: true,
-        FeatureFlags.welcomePageEnabled: true,
-        FeatureFlags.preJoinPageEnabled: true,
-        FeatureFlags.unsafeRoomWarningEnabled: true,
-        FeatureFlags.resolution: FeatureFlagVideoResolutions.resolution720p,
-        FeatureFlags.audioFocusDisabled: true,
-        FeatureFlags.audioMuteButtonEnabled: true,
-        FeatureFlags.audioOnlyButtonEnabled: true,
-        FeatureFlags.calenderEnabled: true,
-        FeatureFlags.callIntegrationEnabled: true,
-        FeatureFlags.carModeEnabled: true,
-        FeatureFlags.closeCaptionsEnabled: true,
-        FeatureFlags.conferenceTimerEnabled: true,
-        FeatureFlags.chatEnabled: true,
-        FeatureFlags.filmstripEnabled: true,
-        FeatureFlags.fullScreenEnabled: true,
-        FeatureFlags.helpButtonEnabled: true,
-        FeatureFlags.inviteEnabled: true,
-        FeatureFlags.androidScreenSharingEnabled: true,
-        FeatureFlags.speakerStatsEnabled: true,
-        FeatureFlags.kickOutEnabled: true,
-        FeatureFlags.liveStreamingEnabled: true,
-        FeatureFlags.lobbyModeEnabled: true,
-        FeatureFlags.meetingNameEnabled: true,
-        FeatureFlags.meetingPasswordEnabled: true,
-        FeatureFlags.notificationEnabled: true,
-        FeatureFlags.overflowMenuEnabled: true,
-        FeatureFlags.pipEnabled: true,
-        FeatureFlags.pipWhileScreenSharingEnabled: true,
-        FeatureFlags.preJoinPageHideDisplayName: true,
-        FeatureFlags.raiseHandEnabled: true,
-        FeatureFlags.reactionsEnabled: true,
-        FeatureFlags.recordingEnabled: true,
-        FeatureFlags.replaceParticipant: true,
-        FeatureFlags.securityOptionEnabled: true,
-        FeatureFlags.serverUrlChangeEnabled: true,
-        FeatureFlags.settingsEnabled: true,
-        FeatureFlags.tileViewEnabled: true,
-        FeatureFlags.videoMuteEnabled: true,
-        FeatureFlags.videoShareEnabled: true,
-        FeatureFlags.toolboxEnabled: true,
-        FeatureFlags.iosRecordingEnabled: true,
-        FeatureFlags.iosScreenSharingEnabled: true,
-        FeatureFlags.toolboxAlwaysVisible: true,
-      },
-      userInfo: JitsiMeetUserInfo(
-        displayName: 'Fresh-Start Company',
-        email: 'fresh-start-dev@gmail.com',
-        avatar:
-            'https://media.licdn.com/dms/image/D4D03AQEGmEfbRTuvNQ/profile-displayphoto-shrink_800_800/0/1712652884077?e=1720051200&v=beta&t=EJ_g-czNv7V_E2h9KxkDxFXbh0LsYGZaemqdi7TMEWE',
-      ),
-    );
-
-    var listener = JitsiMeetEventListener(
-      conferenceJoined: (url) {
-        debugPrint('conferenceJoined: url: $url');
-      },
-      conferenceTerminated: (url, error) {
-        debugPrint('conferenceTerminated: url: $url, error: $error');
-      },
-      conferenceWillJoin: (url) {
-        debugPrint('conferenceWillJoin: url: $url');
-      },
-      participantJoined: (email, name, role, participantId) {
-        debugPrint(
-          'participantJoined: email: $email, name: $name, role: $role, '
-          'participantId: $participantId',
-        );
-        participants.add(participantId!);
-      },
-      participantLeft: (participantId) {
-        debugPrint('participantLeft: participantId: $participantId');
-      },
-      audioMutedChanged: (muted) {
-        debugPrint('audioMutedChanged: isMuted: $muted');
-      },
-      videoMutedChanged: (muted) {
-        debugPrint('videoMutedChanged: isMuted: $muted');
-      },
-      endpointTextMessageReceived: (senderId, message) {
-        debugPrint(
-          'endpointTextMessageReceived: senderId: $senderId, message: $message',
-        );
-      },
-      screenShareToggled: (participantId, sharing) {
-        debugPrint(
-          'screenShareToggled: participantId: $participantId, '
-          'isSharing: $sharing',
-        );
-      },
-      chatMessageReceived: (senderId, message, isPrivate, timestamp) {
-        debugPrint(
-          'chatMessageReceived: senderId: $senderId, message: $message, '
-          'isPrivate: $isPrivate, timestamp: $timestamp',
-        );
-      },
-      chatToggled: (isOpen) => debugPrint('chatToggled: isOpen: $isOpen'),
-      participantsInfoRetrieved: (participantsInfo) {
-        debugPrint(
-          'participantsInfoRetrieved: participantsInfo: $participantsInfo, ',
-        );
-      },
-      readyToClose: () {
-        debugPrint('readyToClose');
-      },
-    );
-    await _jitsiMeetPlugin.join(options, listener);
-  }
-
-  hangUp() async {
-    await _jitsiMeetPlugin.hangUp();
-  }
-
-  setAudioMuted(bool? muted) async {
-    var a = await _jitsiMeetPlugin.setAudioMuted(muted!);
-    debugPrint('$a');
-    setState(() {
-      audioMuted = muted;
-    });
-  }
-
-  setVideoMuted(bool? muted) async {
-    var a = await _jitsiMeetPlugin.setVideoMuted(muted!);
-    debugPrint('$a');
-    setState(() {
-      videoMuted = muted;
-    });
-  }
-
-  sendEndpointTextMessage() async {
-    var a = await _jitsiMeetPlugin.sendEndpointTextMessage(message: 'HEY');
-    debugPrint('$a');
-
-    for (var p in participants) {
-      var b =
-          await _jitsiMeetPlugin.sendEndpointTextMessage(to: p, message: 'HEY');
-      debugPrint('$b');
-    }
-  }
-
-  toggleScreenShare(bool? enabled) async {
-    await _jitsiMeetPlugin.toggleScreenShare(enabled!);
-
-    setState(() {
-      screenShareOn = enabled;
-    });
-  }
-
-  openChat() async {
-    await _jitsiMeetPlugin.openChat();
-  }
-
-  sendChatMessage() async {
-    var a = await _jitsiMeetPlugin.sendChatMessage(message: 'HEY1');
-    debugPrint('$a');
-
-    for (var p in participants) {
-      a = await _jitsiMeetPlugin.sendChatMessage(to: p, message: 'HEY2');
-      debugPrint('$a');
-    }
-  }
-
-  closeChat() async {
-    await _jitsiMeetPlugin.closeChat();
-  }
-
-  retrieveParticipantsInfo() async {
-    var a = await _jitsiMeetPlugin.retrieveParticipantsInfo();
-    debugPrint('$a');
   }
 
   void startCaptureAudio() async {
@@ -236,6 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
         onResult: (result) {
           messageController.text = result.recognizedWords;
         },
+        listenFor: const Duration(minutes: 10),
       );
     } else {
       log('The user has denied the use of speech recognition.');
@@ -243,8 +64,16 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void stopCaptureAudio() async {
+    forceStopped = true;
     speech.stop();
+    forceStopped = false;
     setState(() => isTakingAudioInput = false);
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -261,7 +90,10 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               //
               ElevatedButton(
-                onPressed: join,
+                onPressed: () => HelperMethods.join(
+                  jitsiMeetPlugin: _jitsiMeetPlugin,
+                  participants: participants,
+                ),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
                   foregroundColor: Colors.white,
@@ -272,7 +104,8 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 20),
 
               ElevatedButton(
-                onPressed: hangUp,
+                onPressed: () =>
+                    HelperMethods.hangUp(jitsiMeetPlugin: _jitsiMeetPlugin),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.red,
                   foregroundColor: Colors.white,
@@ -307,29 +140,29 @@ class _HomeScreenState extends State<HomeScreen> {
                 onTapOutside: (event) => FocusScope.of(context).unfocus,
               ),
 
-              const SizedBox(height: 20),
+              // const SizedBox(height: 20),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Set Audio Muted'),
-                  Checkbox(
-                    value: audioMuted,
-                    onChanged: setAudioMuted,
-                  ),
-                ],
-              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Text('Set Audio Muted'),
+              //     Checkbox(
+              //       value: audioMuted,
+              //       onChanged: setAudioMuted,
+              //     ),
+              //   ],
+              // ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Set Video Muted'),
-                  Checkbox(
-                    value: videoMuted,
-                    onChanged: setVideoMuted,
-                  ),
-                ],
-              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Text('Set Video Muted'),
+              //     Checkbox(
+              //       value: videoMuted,
+              //       onChanged: setVideoMuted,
+              //     ),
+              //   ],
+              // ),
 
               // TextButton(
               //   onPressed: sendEndpointTextMessage,
